9b0c815248c73b5fa02241698c0bc614ec57e43a
zador94 <116302825+zador94@users.noreply.github.com>
2023-11-26 19:53:27 +0300

    4.2 lesson

--- test.php
+++ test.php
@@ -2,18 +2,14 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Тестовая страница");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:menu",
-	"left_menu_andru",
-	Array(
-		"ALLOW_MULTI_SELECT" => "Y",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "Y",
-		"MAX_LEVEL" => "2",
-		"MENU_CACHE_GET_VARS" => array(""),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "left",
-		"USE_EXT" => "N"
-	)
+	"bitrix:system.auth.form", 
+	"auth", 
+	array(
+		"COMPONENT_TEMPLATE" => "auth",
+		"REGISTER_URL" => "/user/registration.php",
+		"FORGOT_PASSWORD_URL" => "/user",
+		"PROFILE_URL" => "/user/profile.php",
+		"SHOW_ERRORS" => "N"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ user/registration.php
@@ -0,0 +1,17 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Регистрация");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.register",
+	"",
+	Array(
+		"AUTH" => "Y",
+		"REQUIRED_FIELDS" => array(),
+		"SET_TITLE" => "Y",
+		"SHOW_FIELDS" => array(),
+		"SUCCESS_PAGE" => "",
+		"USER_PROPERTY" => array(),
+		"USER_PROPERTY_NAME" => "",
+		"USE_BACKURL" => "Y"
+	)
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
